5e33a61d22bec19b47984ffd09a7f90555412a78
ghiath2 <ghiathht3@gmail.com>
2022-03-14 13:37:25 +0000

    Add shuffel function to shuffel game cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,5 +20,46 @@ let cardsContainer = document.querySelector(".memory-game-cards");
 let cards = Array.from(cardsContainer.children);
 
 let orderRange = [...Array(cards.length).keys()];
-
 console.log(orderRange);
+shuffle(orderRange);
+console.log(orderRange);
+
+// Add Order Css Property To Game Cards
+
+cards.forEach((card, index) => {
+
+    // Add CSS Order Property
+    card.style.order = orderRange[index];
+  
+  });
+
+ // Shuffle Function
+function shuffle(array) {
+
+    // Settings Vars
+    let current = array.length,
+        temp,
+        random;
+  
+    while (current > 0) {
+  
+      // Get Random Element
+      random = Math.floor(Math.random() * current);
+  
+      // Decrease Length By One
+      current--;
+  
+      // [1] Save Current Element in Stash
+      temp = array[current];
+  
+      // [2] Current Element = Random Element
+      array[current] = array[random];
+  
+      // [3] Random Element = Get Element From Stash
+      array[random] = temp;
+  
+    }
+  
+    return array;
+  
+  }
