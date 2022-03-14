306216b78ef15e52b02bea357f26c7c111899f16
ghiath2 <ghiathht3@gmail.com>
2022-03-14 23:20:07 +0000

    Add comments to css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,111 @@
-document.querySelector(".control-button span").onclick = function () {
+
+  
+  // Select The Start Game Buttun
+
+  document.querySelector(".control-button span").onclick = function () {
+
+    // Prompt Window To Ask For Name 
 
     let yourName = prompt("What's Your Name");
 
+    // If Name Is Empty
+
     if (yourName == null || yourName == "") {
-        
+
+    // Set Name To Unknown  
+
         document.querySelector(".name span").innerHTML = 'Unknown';
+
+    // Name Is Not Empty  
+
     } else {
+
+    // Set Name To Your Name
 
         document.querySelector(".name span").innerHTML = yourName;
     }
 
+    // Remove Splash Screen
+
     document.querySelector(".control-button").remove();
-};
+  };
 
-let duration = 1000;
+  // Effect Duration
 
-let cardsContainer = document.querySelector(".memory-game-cards");
+  let duration = 1000;
 
-let cards = Array.from(cardsContainer.children);
+  // Select Cards Container
 
-let orderRange = [...Array(cards.length).keys()];
+  let cardsContainer = document.querySelector(".memory-game-cards");
 
-shuffle(orderRange);
+  // Creat Array From Game Cards 
 
+  let cards = Array.from(cardsContainer.children);
+
+  // Create Range Of Keys
+
+  let orderRange = [...Array(cards.length).keys()];
 
 // Add Order Css Property To Game Cards
 
 cards.forEach((card, index) => {
 
     // Add CSS Order Property
+
     card.style.order = orderRange[index];
 
     // Add Click Event
+
   card.addEventListener('click', function () {
 
     // Trigger The Flip Card Function
+
     flipCard(card);
 
   });
   
   });
 
-  // Flip Cards Function
+    // Flip Cards Function
 
   function flipCard(selectedCard) {
       
     // Add Class is-flipped
+
   selectedCard.classList.add('is-flipped');
 
-   // Collect All Flipped Cards
+    // Collect All Flipped Cards
+
    let allFlippedCards = cards.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
 
    // If Theres Two Selected Cards
+
    if (allFlippedCards.length === 2) {
        // Stop Clicking Function
+
     stopClicking();
 
      // Check Matched Block Function
+
      checkMatchedCards(allFlippedCards[0], allFlippedCards[1]);
 
   }
   }
   
   // Stop Clicking Function
+
 function stopClicking() {
 
     // Add Class No Clicking on Cards Container
+
     cardsContainer.classList.add('no-clicking');
   
     // Wait Duration
+
     setTimeout(() => {
   
       // Remove Class No Clicking After The Duration
+
       cardsContainer.classList.remove('no-clicking');
   
     }, duration);
@@ -79,6 +113,7 @@ function stopClicking() {
   } 
 
   // Check Matched Cards
+
 function checkMatchedCards(firstCard, secondCard) {
 
     let triesElement = document.querySelector('.tries span');
@@ -114,9 +149,11 @@ function checkMatchedCards(firstCard, secondCard) {
   
 
  // Shuffle Function
+
 function shuffle(array) {
 
     // Settings Vars
+
     let current = array.length,
         temp,
         random;
@@ -124,18 +161,23 @@ function shuffle(array) {
     while (current > 0) {
   
       // Get Random Element
+
       random = Math.floor(Math.random() * current);
   
       // Decrease Length By One
+
       current--;
   
       // [1] Save Current Element in Stash
+
       temp = array[current];
   
       // [2] Current Element = Random Element
+
       array[current] = array[random];
   
       // [3] Random Element = Get Element From Stash
+
       array[random] = temp;
   
     }
